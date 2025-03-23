be534b52e64120b0417e493d800b067ad55090dc
Thomas Peiselt <pi@kulturguerilla.org>
2025-03-23 19:31:57 +0100

    Use mold

--- flake.nix
+++ flake.nix
@@ -38,23 +38,34 @@
         # For `nix build` & `nix run`:
         packages.default = touchscreen-gestures;
         # For `nix develop`:
-        devShell = pkgs.mkShell {
-          inputsFrom = [ touchscreen-gestures ];
-          packages = with pkgs; [
-            clippy
-            rustfmt
-            rust-analyzer
-            cargo-audit
-            cargo-nextest
-            cargo-watch
-          ];
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
-          RUSTC_WRAPPER = "${pkgs.sccache}/bin/sccache";
-          SCCACHE_CACHE_SIZE = "20G";
-        };
+        devShell = pkgs.mkShell.override
+          {
+            stdenv =
+              # use Mold as the linker rather than ld, for faster builds. Mold
+              # to require substantially less memory to link Nexus and its
+              # avoiding swapping on memory-constrained dev systems.
+              pkgs.stdenvAdapters.useMoldLinker
+                # use Clang as the C compiler for all C libraries.
+                # clangStdenv;
+                # stdenv;
+                pkgs.llvmPackages_14.stdenv;
+          }
+          {
+            inputsFrom = [ touchscreen-gestures ];
+            packages = with pkgs; [
+              clippy
+              rustfmt
+              rust-analyzer
+              cargo-audit
+              cargo-nextest
+              cargo-watch
+            ];
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+            RUSTC_WRAPPER = "${pkgs.sccache}/bin/sccache";
+            SCCACHE_CACHE_SIZE = "20G";
+          };
 
-
-        overlays.default = final: prev: {
+          overlays.default = final: prev: {
           touchscreen-gestures = touchscreen-gestures;
         };
       }
